df68edce8dd6684c31d97d39418364d1760dee9c
Shakib Khan <leadwithshakib@gmail.com>
2026-04-22 13:40:29 +0600

    feat: add constants for application download URLs and site configuration metadata

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,7 +1,7 @@
 export const DOWNLOAD_URLS = {
-  WINDOWS: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.2/iMemo-Smart-Clipboard-Windows-0.1.0-Setup.exe',
-  MAC: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.2/iMemo-Smart-Clipboard-Mac-0.1.0.dmg',
-  LINUX: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.2/iMemo-Smart-Clipboard-Linux-0.1.0.AppImage'
+  WINDOWS: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.0/iMemo-Smart-Clipboard-Windows-0.1.0-Setup.exe',
+  MAC: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.0/iMemo-Smart-Clipboard-Mac-0.1.0.dmg',
+  LINUX: 'https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.0/iMemo-Smart-Clipboard-Linux-0.1.0.AppImage'
 };
 
 export const SITE_CONFIG = {
