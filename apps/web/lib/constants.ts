ece3c350d9d6eee04d14ea30622803b21fd92b13
Shakib Khan <leadwithshakib@gmail.com>
2026-04-25 09:35:39 +0600

    chore: release v0.1.8

--- apps/web/lib/constants.ts
+++ apps/web/lib/constants.ts
@@ -1,10 +1,10 @@
 export const DOWNLOAD_URLS = {
   WINDOWS:
-    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.6/iMemo-Smart-Clipboard-Windows-0.1.6-Setup.exe",
+    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.8/iMemo-Smart-Clipboard-Windows-0.1.8-Setup.exe",
   MAC:
-    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.6/iMemo-Smart-Clipboard-Mac-0.1.6.dmg",
+    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.8/iMemo-Smart-Clipboard-Mac-0.1.8.dmg",
   LINUX:
-    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.6/iMemo-Smart-Clipboard-Linux-0.1.6.AppImage",
+    "https://github.com/lwshakib/imemo-smart-clipboard/releases/download/v0.1.8/iMemo-Smart-Clipboard-Linux-0.1.8.AppImage",
 }
 
 export const SITE_CONFIG = {
